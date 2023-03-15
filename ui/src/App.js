9daeda141afef61ff300f70d350ac14ab5609c42
Kevin Mugumya <luwembamugumya@gmail.com>
2023-03-15 23:47:10 +0800

    chore: removed redundant ProtectedRoute

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import CircleSpinner from "./components/CircleSpinner/CircleSpinner";
-import ProtectedRoute from "./auth/protected-route";
 
 const App = () => {
   const { isLoading, isAuthenticated } = useAuth0();
@@ -28,7 +27,11 @@ const App = () => {
       <Route
         path="/callback"
         element={
-          <ProtectedRoute component={<h1>This is your callback page</h1>} />
+          <h1 style={{ padding: "20px" }}>
+            {isAuthenticated
+              ? "You are authenticated."
+              : "You are not authenticated."}
+          </h1>
         }
       />
     </Routes>
