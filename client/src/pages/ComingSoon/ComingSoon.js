14ed94610d75ef235468cb0ed6c8fcae27acabd1
Kevin Mugumya <luwembamugumya@gmail.com>
2023-03-18 08:48:51 +0800

    patch: improved coming soon page

--- client/src/pages/ComingSoon/ComingSoon.js
+++ client/src/pages/ComingSoon/ComingSoon.js
@@ -1,7 +1,8 @@
 import "./style.css";
 import React from "react";
 import { Paper, Typography } from "@mui/material";
-import AlternateTimeline from "./AlternateTimeLine";
+import { BiMailSend } from "react-icons/bi";
+import CustomizedStepper from "./CustomizedStepper";
 
 function ComingSoon() {
   return (
@@ -17,28 +18,66 @@ function ComingSoon() {
           padding: "10px",
         }}
       >
-        <img
-          style={{ maxWidth: "300px" }}
-          src="https://static.wixstatic.com/media/03008d_2282c8a2fb8c4ed4ab4e222d4c01dd9c~mv2.png"
-          alt="logo"
-        />
-        <p
+        <Paper
           style={{
-            fontWeight: "bolder",
-            fontSize: "8vmin",
-            fontFamily: "ubuntu",
-            marginTop: "10px",
+            width: "100%",
+            borderTop: "30px solid red",
+            borderRadius: "20px",
+            padding: "50px",
+            boxShadow:
+              "rgb(0 0 0 / 10%) 0px 0px 15px -3px, rgb(0 0 0 / 10%) 0px 4px 6px -10px",
           }}
+          elevation={3}
         >
-          Democratising Uganda's Health Data
-        </p>
+          <img
+            style={{
+              maxWidth: "200px",
+            }}
+            src="https://static.wixstatic.com/media/03008d_2282c8a2fb8c4ed4ab4e222d4c01dd9c~mv2.png"
+            alt="logo"
+          />
+          <p
+            style={{
+              fontWeight: "bolder",
+              fontSize: "7vmin",
+              fontFamily: "Inter",
+              marginTop: "10px",
+              color: "black",
+            }}
+          >
+            <span
+              style={{
+                color: "rgb(102, 102, 102)",
+                fontFamily: "Inter",
+                fontSize: "7vmin",
+              }}
+            >
+              Democratising{" "}
+            </span>
+            Uganda's Health Data
+          </p>
+          <p
+            style={{
+              fontSize: "2vmin",
+              fontFamily: "Inter",
+              marginTop: "20px",
+              color: "grey",
+            }}
+          >
+            For the Public, Research Institutions and Government Organizations.
+          </p>
+        </Paper>
         <p
           style={{
-            marginTop: "50px",
+            marginTop: "30px",
             fontWeight: "800",
             fontSize: "4vmin",
             fontFamily: "ubuntu",
-            color: "#D81100",
+            color: "#ED5840",
+            background:
+              "-webkit-linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
           }}
         >
           Coming Soon
@@ -49,14 +88,41 @@ function ComingSoon() {
             fontSize: "15px",
             marginTop: "10px",
             marginBottom: "20px",
-            color: "grey",
-            fontWeight: "500",
+            color: "black",
+            fontWeight: "800",
           }}
           variant="h1"
         >
           Read our White Paper to learn more
         </Typography>
-        <AlternateTimeline />
+        <div style={{ marginTop: "30px" }}>
+          <CustomizedStepper />
+        </div>
+        <BiMailSend
+          style={{ marginTop: "50px", color: "grey" }}
+          fontSize={30}
+        />
+        <div>
+          <a
+            href="mailto:sonia.salim@icloud.com"
+            style={{
+              color: "grey",
+              fontSize: "12px",
+              marginRight: "20px",
+            }}
+          >
+            Sonia Salim
+          </a>
+          <a
+            href="mailto:luwembamugumya@gmail.com"
+            style={{
+              color: "grey",
+              fontSize: "12px",
+            }}
+          >
+            Kevin Mugumya
+          </a>
+        </div>
       </div>
     </div>
   );
